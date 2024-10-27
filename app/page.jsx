333245a4ab5bf49d3de999402b74074231e51e0e
OmarAshraf1911 <omarashraf.90.5.370@gmail.com>
2024-10-27 14:40:32 +0300

    upload files

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,15 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import HomeProperties from "@/components/HomeProperties";
+// import connectDB from "@/config/database";
+
 const HomePage = () => {
+  // await connectDB();
   return (
     <>
       <Hero />
       <InfoBoxes />
+      <HomeProperties />
     </>
   );
 };
