4bfe57acfa84becb7a82aca2d8ca28f3b60d3a40
OmarAshraf1911 <omarashraf.90.5.370@gmail.com>
2024-11-14 17:21:48 +0200

    upload files

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,10 @@ import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import HomeProperties from "@/components/HomeProperties";
 import FeaturedProperties from "@/components/FeaturedProperties";
-import Head from "next/head";
 
 const HomePage = () => {
   return (
     <>
-      <Head>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      </Head>
       <Hero />
       <InfoBoxes />
       <FeaturedProperties />
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,68 +1,24 @@
-"use client";
 import Image from "next/image";
-import Link from "next/link";
-import { useSession } from "next-auth/react";
-import { useState, useEffect } from "react";
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
 import profileDefault from "@/assets/images/profile.png";
-import Spinner from "@/components/Spinner";
-import { toast } from "react-toastify";
+import ProfileProperties from "@/components/ProfileProperties";
+import { convertToSerializeableObject } from "@/utils/convertToObject";
 
-const ProfilePage = () => {
-  const { data: session } = useSession();
-  const profileImage = session?.user?.image;
-  const profileName = session?.user?.name;
-  const profileEmail = session?.user?.email;
+const ProfilePage = async () => {
+  await connectDB();
 
-  const [properties, setProperties] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const sessionUser = await getSessionUser();
 
-  useEffect(() => {
-    const fetchUserProperties = async (userId) => {
-      if (!userId) {
-        return;
-      }
+  const { userId } = sessionUser;
 
-      try {
-        const res = await fetch(`/api/properties/user/${userId}`);
-        if (res.status === 200) {
-          const data = await res.json();
-          setProperties(data);
-        }
-      } catch (error) {
-        console.error("Error fetching user properties", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  if (!userId) {
+    throw new Error("User ID is required");
+  }
 
-    // Fetch user properties when session is available
-    if (session?.user?.id) {
-      fetchUserProperties(session.user.id);
-    }
-  }, [session]);
-
-  const handleDeleteProperty = async (propertyId) => {
-    const confirmed = window.confirm("Are you sure you want to delete");
-    if (!confirmed) return;
-    try {
-      const res = await fetch(`/api/properties/${propertyId}`, {
-        method: "DELETE",
-      });
-      if (res.status === 200) {
-        // Remove the property from the state
-        const updatedProperties = properties.filter(
-          (property) => property._id !== propertyId
-        );
-        setProperties(updatedProperties);
-        toast.success("Property deleted successfully");
-      } else {
-        toast.error("failed to delete");
-      }
-    } catch (error) {
-      console.log(error);
-      toast.error("failed to delete");
-    }
-  };
+  const propertiesDocs = await Property.find({ owner: userId }).lean();
+  const properties = propertiesDocs.map(convertToSerializeableObject);
 
   return (
     <section className="bg-blue-50">
@@ -70,11 +26,11 @@ const ProfilePage = () => {
         <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
           <h1 className="text-3xl font-bold mb-4">Your Profile</h1>
           <div className="flex flex-col md:flex-row">
-            <div className="md:w-1/4 mx-20 mt-1 0">
+            <div className="md:w-1/4 mx-20 mt-10">
               <div className="mb-4">
                 <Image
                   className="h-32 w-32 md:h-48 md:w-48 rounded-full mx-auto md:mx-0"
-                  src={profileImage || profileDefault}
+                  src={sessionUser.user.image || profileDefault}
                   width={200}
                   height={200}
                   alt="User"
@@ -82,57 +38,21 @@ const ProfilePage = () => {
               </div>
 
               <h2 className="text-2xl mb-4">
-                <span className="font-bold block">Name: </span> {profileName}
+                <span className="font-bold block">Name: </span>{" "}
+                {sessionUser.user.name}
               </h2>
               <h2 className="text-2xl">
-                <span className="font-bold block">Email: </span> {profileEmail}
+                <span className="font-bold block">Email: </span>{" "}
+                {sessionUser.user.email}
               </h2>
             </div>
 
             <div className="md:w-3/4 md:pl-4">
               <h2 className="text-xl font-semibold mb-4">Your Listings</h2>
-              {!loading && properties.length === 0 && (
+              {properties.length === 0 ? (
                 <p>You have no property listings</p>
-              )}
-              {loading ? (
-                <Spinner loading={loading} />
               ) : (
-                properties.map((property) => (
-                  <div key={property._id} className="mb-10">
-                    <Link href={`/properties/${property._id}`}>
-                      <Image
-                        className="h-32 w-full rounded-md object-cover"
-                        src={property.images[0]}
-                        alt="Property"
-                        width={500}
-                        height={100}
-                        priority={true}
-                      />
-                    </Link>
-                    <div className="mt-2">
-                      <p className="text-lg font-semibold">{property.name}</p>
-                      <p className="text-gray-600">
-                        Address: {property.location.street}{" "}
-                        {property.location.city} {property.location.state}
-                      </p>
-                    </div>
-                    <div className="mt-2">
-                      <Link
-                        href={`/properties/${property._id}/edit`}
-                        className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-                      >
-                        Edit
-                      </Link>
-                      <button
-                        onClick={() => handleDeleteProperty(property._id)}
-                        className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-                        type="button"
-                      >
-                        Delete
-                      </button>
-                    </div>
-                  </div>
-                ))
+                <ProfileProperties properties={properties} />
               )}
             </div>
           </div>
